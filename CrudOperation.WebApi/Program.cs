de9b888ba3027648f093b855ece85dc431cf7711
Aslam Shaikh <beingaslam@outlook.in>
2023-08-16 03:52:54 +0530

    Added Full Support of Serilog AspNetCore & MsSqlServer Sink

--- CrudOperation.WebApi/Program.cs
+++ CrudOperation.WebApi/Program.cs
@@ -2,6 +2,7 @@ using CrudOperation.DataAccess;
 using CrudOperation.DataAccess.UnitOfWork;
 using CrudOperation.Domain.Interfaces;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
 
 namespace CrudOperation.WebApi
 {
@@ -9,35 +10,54 @@ namespace CrudOperation.WebApi
     {
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
+            Log.Logger = new LoggerConfiguration().CreateBootstrapLogger();
 
-            // Add services to the container.
-            builder.Services.AddControllers();
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            builder.Services.AddDbContext<AppDbContext>(options =>
-                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-            builder.Services.AddTransient<IUnitOfWork, UnitOfWork>();
-
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
+            try
             {
-                app.UseSwagger();
-                app.UseSwaggerUI();
+                var builder = WebApplication.CreateBuilder(args);
+                builder.Host.UseSerilog((context, configuration) =>
+                    configuration.ReadFrom.Configuration(context.Configuration));
+
+                // Add services to the container.
+                builder.Services.AddControllers();
+                builder.Services.AddEndpointsApiExplorer();
+                builder.Services.AddSwaggerGen();
+
+                builder.Services.AddDbContext<AppDbContext>(options =>
+                    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+                builder.Services.AddTransient<IUnitOfWork, UnitOfWork>();
+
+
+                var app = builder.Build();
+
+                // Configure the HTTP request pipeline.
+                if (app.Environment.IsDevelopment())
+                {
+                    app.UseSwagger();
+                    app.UseSwaggerUI();
+                }
+
+                app.UseSerilogRequestLogging();
+
+                app.UseHttpsRedirection();
+
+                app.UseAuthorization();
+
+                app.MapControllers();
+
+                app.Run();
             }
 
-            app.UseHttpsRedirection();
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Application terminated unexpectedly");
+            }
 
-            app.UseAuthorization();
-
-            app.MapControllers();
-
-            app.Run();
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
     }
 }
